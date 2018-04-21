26720bca1036904338b351c9a440a833690d8c2f
Ewerton <shakamaiden@gmail.com>
2018-04-22 00:09:40 +0200

    Added sounds to theproject, Functional Pause menu with animations, New Sponsors

---
+++ Assets/Scripts/MenuAnimations.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MenuAnimations : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+    Animator animator;
+
+    // Use this for initialization
+    void Start () {
+        animator = GetComponent<Animator>();
+        animator.SetBool("OnHover", false);
+
+    }
+
+    public void OnHover() {
+        animator.SetBool("OnHover", true);
+    }
+
+    public void OnHoverExit() {
+        animator.SetBool("OnHover", false);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData) {
+        print("Pointer over");
+        animator.SetBool("OnHover", true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData) {
+        print("Pointer left");
+        animator.SetBool("OnHover", false);
+    }
+}
---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+
+    public bool gamePaused;
+    GameObject pauseMenu;
+
+    
+
+    void Start() {
+        pauseMenu = GameObject.FindGameObjectWithTag("PauseCanvas");
+        pauseMenu.SetActive(false);
+        gamePaused = false;
+        
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKey(KeyCode.P)) {
+            if (gamePaused) {
+                Resume();
+            } else {
+                Pause();
+            }
+        }
+	}
+
+    public void Resume() {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        gamePaused  = false;
+    }
+
+    void Pause() {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        gamePaused = true;
+    }
+
+    public void MainMenu() {
+        // Got to main menu
+        SceneManager.LoadScene("NameOfTheScene");
+    }
+    public void Exit() {
+        Application.Quit();
+    }
+
+
+}
