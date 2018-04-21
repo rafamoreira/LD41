f2b553e5fb9d86711ea227abe1fa2d337f15d88e
Ewerton <shakamaiden@gmail.com>
2018-04-21 21:32:58 +0200

    Set Up animations

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -13,13 +13,18 @@ public class Controller : MonoBehaviour
     public float movementSpeed;
     public float punchDelay;
 
+    Animator animator;
+    Vector3 myScale;
 
     // Use this for initialization
     void Start ()
     {
+        animator = GetComponent<Animator>();
+        animator.SetBool("isIdleBottom", true);
         myRB = GetComponent<Rigidbody2D>();
         pCondition = GetComponent<PlayerCondition>();
         punchTimer = 0;
+        myScale = transform.localScale;
 	}
 
     private void Update()
@@ -30,21 +35,37 @@ public class Controller : MonoBehaviour
         }
 
         punchTimer -= Time.deltaTime;
+
+        if(Input.GetAxis("Vertical") != 0) {
+            animator.SetBool("Vertical", true);
+            if(Input.GetAxis("Vertical") < 0) {
+                animator.SetBool("VerticalBottom", true);
+            } else {
+                animator.SetBool("VerticalBottom", false);
+            }
+        } else if (Input.GetAxis("Horizontal") != 0) {
+            animator.SetBool("VerticalBottom", false);
+            animator.SetBool("Vertical", false);
+            animator.SetBool("Horizontal", true);
+            if (Input.GetAxis("Horizontal") < 0) {                
+                transform.localScale = new Vector3(myScale.x * -1, myScale.y, myScale.z);
+            } else {
+                transform.localScale = myScale;
+            }        
+        } else {
+            animator.SetBool("Vertical", false);
+            animator.SetBool("Horizontal", false);
+        }
     }
 
     // Update is called once per frame
     void FixedUpdate ()
     {
-        Vector2 movement = Vector2.zero;
-        if (Input.GetAxis("Horizontal") != 0)
-        {
-            movement.x = Input.GetAxis("Horizontal") * movementSpeed;
-        }
+        Vector2 movement = Vector2.zero;   
 
-        if (Input.GetAxis("Vertical") != 0)
-        {
-            movement.y = Input.GetAxis("Vertical") * movementSpeed;
-        }
+        movement.x = Input.GetAxis("Horizontal") * movementSpeed;            
+
+        movement.y = Input.GetAxis("Vertical") * movementSpeed;        
 
         myRB.MovePosition(myRB.position + movement * Time.fixedDeltaTime);
     }
